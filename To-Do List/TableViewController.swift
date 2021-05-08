c73701d884852bbeab579c7886a8a9c26201667b
First name hg <cistakovvasilijaleksandrovic@MacBook-Pro-Cistakov.local>
2021-05-08 20:31:19 +0300

    08.05

--- To-Do List/TableViewController.swift
+++ To-Do List/TableViewController.swift
@@ -6,19 +6,20 @@
 //
 
 import UIKit
+import CoreData
 
 class TableViewController: UITableViewController {
     
-    var tasks: [String] = [" "]
+    var tasks: [Task] = [ ]
     
-    @IBAction func saveTask(_ sender: UIBarButtonItem) {
+    @IBAction  func saveTask(_ sender: UIBarButtonItem) {
         
         let allertContraler = UIAlertController(title: "New Task ", message: "Pleace add a new task ", preferredStyle: .alert)
         
         let saveAction = UIAlertAction(title: "Save", style: .default) { (action) in
             let tf = allertContraler.textFields?.first
             if let newTask = tf?.text {
-                self.tasks.insert(newTask, at: 0)
+                self.saveTask(withTitle : newTask)
                 self.tableView.reloadData()
             }
         }
@@ -33,35 +34,87 @@ class TableViewController: UITableViewController {
         present(allertContraler, animated: true, completion: nil)
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    private func saveTask(withTitle title: String) {
+        
+        let context = getContext()
+        
+        guard let entity = NSEntityDescription.entity(forEntityName: "Task", in: context)  else { return }
+        
+        let taskObject = Task(entity: entity, insertInto: context)
+        taskObject.title = title
+        
+        do{
+            try context.save()
+            tasks.append(taskObject)
+        } catch let error as NSError{
+            print(error.localizedDescription)
+        }
+    }
+    
+    
+    
+    private func getContext() ->  NSManagedObjectContext {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let context = getContext()
+        
+        let fecthRequestr: NSFetchRequest<Task> = Task.fetchRequest()
+        let sortDescriptor =  NSSortDescriptor (key: "title", ascending: true)
+        fecthRequestr.sortDescriptors = [sortDescriptor]
+        
+        do {
+            tasks = try context.fetch(fecthRequestr )
+        } catch let error as NSError {
+            
+        }
     }
 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        let context = getContext()
+//        let fecthRequestr: NSFetchRequest<Task> = Task.fetchRequest()
+//        if let reults = try? context.fetch(fecthRequestr){
+//            for object in reults {
+//                context.delete(object)
+//            }
+//        }
+//
+//        do {
+//            try context.save()
+//        } catch let error as NSError{
+//            print(error.localizedDescription)
+//        }
+    }
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return tasks.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-
-        cell.textLabel?.text = tasks[indexPath.row]
-
+        
+        let task = tasks[indexPath.row]
+        cell.textLabel?.text = task.title
+        
         return cell
     }
-  
+    
 }
